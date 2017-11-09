668549a4f000066761a76f9e9db58b5d53502603
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-11-09 15:42:08 -0500

    mask out sb2

--- dense_gas_fraction/density_PDFs.py
+++ dense_gas_fraction/density_PDFs.py
@@ -13,6 +13,7 @@ import reproject
 import numpy as np
 import matplotlib.pyplot as plt
 import aplpy
+import pyregion
 
 sma_path = os.path.expanduser('~/Dropbox/SMA_CMZ/CMZoom_Images/November17_continuum_fits/')
 herschel_path = os.path.expanduser('~/Dropbox/SMA_CMZ_FITS_files/')
@@ -41,7 +42,7 @@ smaobserved_projto_herschel,_ = reproject.reproject_interp((sma_observed,
 smaobserved_projto_herschel[np.isnan(smaobserved_projto_herschel)] = 0
 smaobserved_projto_herschel = smaobserved_projto_herschel.astype('bool')
 
-catalog = Table.read(os.path.join(catalog_path, 'mosaic_Nov2017_pruned_v1_datatab.fits'))
+catalog = Table.read(os.path.join(catalog_path, 'mosaic_Nov2017_Jy_per_Ster.fits_datatab_v3.fits'))
 
 colwcs = wcs.WCS(column_fh[0].header)
 pix = colwcs.wcs_world2pix(catalog['GLON'], catalog['GLAT'], 0)
@@ -56,8 +57,10 @@ catalog.write(os.path.join(catalog_path,
 column_masked = column_fh[0].data
 column_masked[~smaobserved_projto_herschel] = np.nan
 
-colmin = np.nanmin(column_masked)
-colmax = np.nanmax(column_masked)
+sb2_mask = pyregion.open(os.path.join(catalog_path, 'sgrb2mask.reg')).get_mask(column_fh[0])
+
+colmin = np.nanmin(column_masked[~sb2_mask])
+colmax = np.nanmax(column_masked[~sb2_mask])
 
 
 smasourcemask = np.zeros_like(smaobserved_projto_herschel, dtype='bool')
@@ -68,7 +71,7 @@ herschel_beamsize_pixels = (25*u.arcsec / np.mean(wcs.utils.proj_plane_pixel_sca
 # make a small radial mask
 npix = int(np.ceil(herschel_beamsize_pixels))
 yy,xx = np.indices([npix*2]*2, dtype='float')
-rad = ((yy-npix)**2 + (xx-npix)**2)**0.5
+rad = ((yy-npix+0.5)**2 + (xx-npix+0.5)**2)**0.5
 radmask = rad < herschel_beamsize_pixels
 
 pb = ProgressBar(len(catalog))
@@ -80,26 +83,32 @@ for row,(cx,cy) in zip(catalog, zip(*pix)):
 
 
 
+
+
+
 # Visually inspect mask on the Herschel file with the mask as a contour
 plt.style.use('seaborn-colorblind')
 plt.rcParams.update({'font.size': 16}) #set fontsize
 
-fig = aplpy.FITSFigure(column_file)
+fig = aplpy.FITSFigure(column_file, figure=plt.figure(1))
 
-fig.show_colorscale(cmap='Greys_r',vmin=colmin,vmax=colmax)
+fig.show_colorscale(cmap='Greys_r', vmin=colmin, vmax=colmax)
 fig.add_colorbar()
 fig.colorbar.set_width(0.3)
 fig.set_theme('publication')
 #fig.set_tick_labels_format(xformat='ddd.d', yformat='dd.d')
 
 # Plot contour of mask ...
-fig.show_contour(smaobserved_projto_herschel.astype('int'), linewidths=[1],
+fig.show_contour(smaobserved_projto_herschel.astype('int'), linewidths=[0.5],
                  colors=['b'], levels=[0])
 
-fig.show_contour(smasourcemask.astype('int'), linewidths=[1],
+fig.show_contour(smasourcemask.astype('int'), linewidths=[0.5],
                  colors=['r'], levels=[0])
 
-fig.recenter(0,0,width=2,height=0.75)
+fig.show_contour(sb2_mask.astype('int'), linewidths=[0.5],
+                 colors=['g'], levels=[0])
+
+fig.recenter(0.25,0,width=3,height=0.75)
 
 plt.savefig(os.path.join(figure_path, 'mask_on_herschelcolumn.pdf'),
             format='pdf', dpi=100, bbox_inches='tight')
@@ -107,29 +116,32 @@ plt.savefig(os.path.join(figure_path, 'mask_on_herschelcolumn.pdf'),
 
 # Plot Histogram
 plt.rcParams.update({'font.size': 24}) #set fontsize
-histfig = plt.figure(2,figsize=(13,6))
+histfig = plt.figure(3,figsize=(13,6))
+histfig.clf()
 ax = histfig.gca()
 plt.ylabel('Number of Pixels')
 plt.xlabel('Column Density N(H$_2$) [cm$^{-2}$]')
 
 #bins = np.logspace(22.9,23.9,100)
-bins = np.logspace(np.log10(colmin),np.log10(colmax),100)
-plt.hist(column_masked[np.isfinite(column_masked)], bins,
-         color='gray',alpha=0.7, log='True',
-         label='Full cloud')
-plt.hist(column_masked[smasourcemask & np.isfinite(column_masked)], bins, alpha=0.7, log='True',
-         label='SMA Sources')
-#plt.hist(G1602[3], bins, color='C0',alpha=0.7, log='True', label=source)
-#plt.hist(sourcecol[3], bins, color='magenta', alpha=0.7, log='True', label=source)
+bins = np.logspace(np.log10(colmin), np.log10(colmax),100)
+h1,l1,p1 = ax.hist(column_masked[np.isfinite(column_masked) & ~sb2_mask],
+                   bins, color='gray',alpha=0.7, log=False,
+                   label='Full cloud')
+h2,l2,p2 = ax.hist(column_masked[smasourcemask & np.isfinite(column_masked) &
+                                 ~sb2_mask], bins, alpha=0.7, log=False,
+                   label='SMA Sources')
 
-
-#plt.hist(dendro,bins, color='cyan', alpha=0.7, log='True', label='Dendrogram Sources')
+ax3 = ax.twinx()
+midpts = (l1[:-1] + l1[1:])/2.
+ax3.plot(midpts, h2/h1, linestyle='-', color='k', linewidth=0.5, zorder=-21,)
+ax3.set_ylabel("Fraction of pixels with stars")
+ax3.set_ylim(0,1)
 
 ax.set_xscale("log")
 #ax.set_xlim(10**22.9, 10**23.9)
-ax.set_xlim(colmin, colmax)
-#ax.set_ylim(1,200)
-ax.set_ylim(1,5000)
+ax.set_xlim(1e22, colmax)
+ax.set_ylim(1,200)
+#ax.set_ylim(1,5000)
 plt.gcf().subplots_adjust(bottom=0.2) #make room for x-axis
 
 legend = plt.legend(loc='upper left', shadow=False, fontsize=18)#'x-large')
@@ -140,3 +152,15 @@ plt.savefig(os.path.join(figure_path, 'column_histograms.pdf'),format='pdf',
             dpi=100, bbox_inches='tight')
 
 plt.show()
+
+cdffig = plt.figure(2)
+cdffig.clf()
+ax = cdffig.gca()
+
+ax.plot(sorted(catalog['ColumnDensity']), np.arange(len(catalog))/len(catalog))
+ax.set_xlabel("Column Density [cm$^{-2}$]")
+ax.set_ylabel("Cumulative Fraction")
+ax.set_xscale('log')
+
+plt.savefig(os.path.join(figure_path, 'cdf_of_column_density.pdf'),format='pdf',
+            dpi=100, bbox_inches='tight')
